c0306e6784e0c29f51151ea1a6cac47648eb9923
Tom Barnes <barnes.thomas.c@gmail.com>
2018-12-16 17:20:02 -0500

    Updates

---
+++ precache-manifest.d8e5524889552f64c03d83ee7e1932bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44eac2e6607a6c7660d2",
+    "url": "/hot-knives-world/static/js/runtime~main.44eac2e6.js"
+  },
+  {
+    "revision": "f9e1925aaf63cb152232",
+    "url": "/hot-knives-world/static/js/main.f9e1925a.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/hot-knives-world/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "f9e1925aaf63cb152232",
+    "url": "/hot-knives-world/static/css/main.044e7726.chunk.css"
+  },
+  {
+    "revision": "faa28e6b0c7d9a597287a265e5bb9015",
+    "url": "/hot-knives-world/index.html"
+  }
+];
